423281c425118312845d386556233556c1bc855a
Anubhuti Singh <sanubhuti201@gmail.com>
2026-08-24 19:08:32 +0530

    Time: 3 ms (29.59%), Space: 8.5 MB (54.58%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,12 +1,15 @@
-class Solution {                      
+class Solution {
 public:
     int reverse(int x) {
-        int r=0;      // decleare r 
-        while(x){
-         if (r>INT_MAX/10 || r<INT_MIN/10) return 0; // check 32 bit range if r is outside the range then return 0 
-         r=r*10+x%10; // find remainder and add its to r
-         x=x/10;     // Update the value of x
-        } 
-        return r;  // if r in the 32 bit range then return r
+        int revnum=0;
+        while(x != 0){
+            int lastDigit = x%10;
+            if ((revnum > INT_MAX / 10) || (revnum < INT_MIN / 10)){
+                return 0;
+            }
+            revnum = revnum * 10 + lastDigit;
+            x= x/10;
+        }
+        return revnum;
     }
-}; 
+};
